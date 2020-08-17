1b88515c4faaffe5148c4b847e9ac2d0d0cb2a7d
Ирина Левдик <levdik_i@ukr.net>
2020-08-17 11:50:38 +0300

    Create CodeRepository, add ,gitignore

--- DataAccess/Repositories/CodeRepository.cs
+++ DataAccess/Repositories/CodeRepository.cs
@@ -1,10 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using DataAccess.Models;
+using System.Linq;
 
 namespace DataAccess.Repositories
 {
     public class CodeRepository
     {
+        #region 
+
+        private readonly TestContext context;
+
+        #endregion
+
+        public CodeRepository(TestContext context)
+        {
+            this.context = context;
+        }
+
+        public void Create(Code item)
+        {
+            context.Codes.Add(item);
+        }
+
+        public void Update(Code item)
+        {
+            context.Codes.Update(item);
+        }
+
+        public Code Get(int id)
+        {
+            return context.Codes
+                .FirstOrDefault(p => p.Id == id);
+        }
+
+        public IQueryable<Code> GetAll()
+        {
+            return context.Codes;
+        }
     }
 }
