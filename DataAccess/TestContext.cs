15e0fd1871dbe42be15e8d667d6ef6aa7c70f269
Ирина Левдик <levdik_i@ukr.net>
2020-08-17 11:40:51 +0300

    Add context

--- DataAccess/TestContext.cs
+++ DataAccess/TestContext.cs
@@ -1,8 +1,15 @@
-﻿using System;
+﻿using DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess
 {
-    public class TestContext
+    public class TestContext : DbContext
     {
+        public DbSet<Code> Codes { get; set; }
+        public TestContext(DbContextOptions<TestContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
     }
 }
+
