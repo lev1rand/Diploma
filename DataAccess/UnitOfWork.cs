70e40edfb673cd9dd90aba72db01f8d3162a3c38
Ирина Левдик <levdik_i@ukr.net>
2020-08-17 11:55:11 +0300

    Add UnitOfWork

--- DataAccess/UnitOfWork.cs
+++ DataAccess/UnitOfWork.cs
@@ -1,10 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using DataAccess.Repositories;
+using System;
 
 namespace DataAccess
 {
     public class UnitOfWork
     {
+        #region privateMembers
+
+        private TestContext context;
+
+        private CodeRepository codeRepository;
+
+        private bool isDisposed;
+
+        #endregion
+
+        public CodeRepository Codes
+        {
+            get
+            {
+                if (codeRepository == null)
+                    codeRepository = new CodeRepository(context);
+
+                return codeRepository;
+            }
+        }
+
+        public UnitOfWork(TestContext context)
+        {
+            this.context = context;
+            isDisposed = false;
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!isDisposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+
+                isDisposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
     }
 }
